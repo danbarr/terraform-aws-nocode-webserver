3678b02f40912bc921ab2c5bb4be618b509d7aa6
Dan Barr <dan@dbarr.com>
2022-10-24 18:32:17 -0400

    Add product to outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,8 @@ output "app_url" {
 output "ami_id" {
   value = aws_instance.hashicafe.ami
 }
+
+output "product" {
+  description = "The product which was randomly selected."
+  value       = var.hashi_products[random_integer.product.result].name
+}
